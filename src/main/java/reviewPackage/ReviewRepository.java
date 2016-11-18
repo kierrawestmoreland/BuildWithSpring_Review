ef2ccc71159acd70022e579068b2fe52aec296f4
We Can Code IT <you@example.com>
2016-11-18 14:46:34 -0500

    changed repository to crud repository interface

--- src/main/java/reviewPackage/ReviewRepository.java
+++ src/main/java/reviewPackage/ReviewRepository.java
@@ -1,44 +1,7 @@
 package reviewPackage;
 
-import java.util.Collection;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.data.repository.CrudRepository;
 
-import org.springframework.stereotype.Component;
+public interface ReviewRepository extends CrudRepository<Review_Model, Long> {
 
-@Component
-public class ReviewRepository {
-	
-	private Map<Long, Review_Model> Review_ModelById;
-	
-	public ReviewRepository(){
-		
-		Review_ModelById = new HashMap<Long, Review_Model>();
-		Review_Model Review_Model1 = new Review_Model(1, "JavaScript", "Kierra", "MAjhhrhewowbddmdjbdejwejdbmsdm dsm dsm d sms dewmnejdmsm", new Date(2016,10,23));
-		
-		Review_ModelById.put(Review_Model1.getId(), Review_Model1);
-		
-		
-        Review_Model Review_Model2 = new Review_Model(2, "JQuery", "Matthew", "MAjhhrhewowbddmdjbdejwejdbmsdm dsm dsm d sms dewmnejdmsm", new Date(2016,8,20));
-		
-		Review_ModelById.put(Review_Model2.getId(), Review_Model2);
-		
-		
-        Review_Model Review_Model3 = new Review_Model(3, "JavaScript", "Seth", "MAjhhrhewowbddmdjbdejwejdbmsdm dsm dsm d sms dewmnejdmsm", new Date(2016,10,23));
-		
-		Review_ModelById.put(Review_Model3.getId(), Review_Model3);
-		
-		
-		
-		
-	}
-    public Review_Model findById(long id){
-		return Review_ModelById.get(id);
-    	
-    }
-	
-	public Collection<Review_Model> findAll() {
-		return Review_ModelById.values();
-	}
 }
--- src/main/java/reviewPackage/Review_Controller2.java
+++ src/main/java/reviewPackage/Review_Controller2.java
@@ -1,6 +1,7 @@
 package reviewPackage;
 
-//import javax.annotation.Resource;
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,15 +10,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 	@Controller
 	public class Review_Controller2 {
 
-	    @RequestMapping("/Review_View")//html file
-	    public String Review_View(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-	        model.addAttribute("name", name);
-	        return "Review_View";
+	@Resource
+	private ReviewRepository repository;
+
+		
+	    @RequestMapping("/reviewPackage")//package name
+	    public String displayReview(@RequestParam("id") long id, Model model) {
+	    	Review_Model review = repository.findOne(id);
+			model.addAttribute("selectedReview", review);
+	    	return "review_View";
 	    }
-	    
-	    @RequestMapping("/Review_View")//html file
-	    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-	        model.addAttribute("name", name);
-	        return "greeting";
+	   
+	    @RequestMapping("/reviewPackALL")
+	    public String displayAll(Model model){
+	    	Iterable<Review_Model> reviews = repository.findAll();
+	    	model.addAttribute("reviews", reviews);
+	    	return "all-reviews";
 	    }
 }
