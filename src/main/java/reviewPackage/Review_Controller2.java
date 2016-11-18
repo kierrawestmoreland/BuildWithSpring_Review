db47a4383ad4f3d59f5a084a929229185aceb987
We Can Code IT <you@example.com>
2016-11-18 12:37:21 -0500

    added Review Controller, html, model files

--- src/main/java/reviewPackage/Review_Controller2.java
+++ src/main/java/reviewPackage/Review_Controller2.java
@@ -1,5 +1,23 @@
 package reviewPackage;
 
-public class Review_Controller2 {
+//import javax.annotation.Resource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+	@Controller
+	public class Review_Controller2 {
+
+	    @RequestMapping("/Review_View")//html file
+	    public String Review_View(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+	        model.addAttribute("name", name);
+	        return "Review_View";
+	    }
+	    
+	    @RequestMapping("/Review_View")//html file
+	    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+	        model.addAttribute("name", name);
+	        return "greeting";
+	    }
 }
--- src/main/java/reviewPackage/Review_Model.java
+++ src/main/java/reviewPackage/Review_Model.java
@@ -1,15 +1,57 @@
 package reviewPackage;
 
-import java.sql.Date;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 
+@Entity
 
 public class Review_Model {
 
+	@Id //The id property is annotated with @Id 
+	    //so that JPA will recognize it as the object’s ID. 
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	//The id property is also annotated with @GeneratedValue 
+	//to indicate that the ID should be generated automatically.
+	
 	private long id;
 	private String title;
 	private Date date;
 	private String author;
 	private String content;
 	
+	public Review_Model(long id, String title, String author, String content, Date date) {
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.content = content;
+		this.date = date;
+		//constructor method
+	}
+	
+	public long getId(){
+		return id;
+	}
+	
+	public String getTitle(){
+		return title;
+	}
+	
+	public String getAuthor(){
+		return author;
+	}
+	
+	public String getContent(){
+		return content;
+	}
+	
+	public Date getDate(){
+		return date;
+	}
+	//point of this class is to hold the data for the IDs
+
 }
